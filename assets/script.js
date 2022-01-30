e9dbf70c39d710066353372e86d6e0a53c19618f
fuadmuradov <1fuad.muradov@gmail.com>
2022-01-30 14:37:14 +0400

    first commit

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+// var text = prompt("Enter your name")
+
+// console.log(text)
+
+// '' && console.log("first")
+// 5 && console.log("second")
+// 5==true || 6 && console.log("third")
+// 8 || '' && console.log("fourth")
+
+// function test(a){
+//     console.log("fuad deyir ki : " + a)
+// }
+
+// test("ha ha aaaaahaa")
+
+// function sum(a=0, b){
+//     return a+b
+// }
+
+// console.log(sum(1))
+
+// function argNumbers(){
+// let argArr = Array.from(arguments)
+// return argArr
+// }
+
+// console.log(argNumbers(1,2,3,4,5,6,7))
+
+// let arrowfunc = (a, b, c) => {
+//     return a+b+c
+// }
+
+// console.log(arrowfunc(1,2,3))
+
+// let func2 = a => {console.log(a)}
+
+// console.log(func2(12))  
+ 
+
+// let func5 = age =>({age});
+// console.log(func5(15))
+
+
